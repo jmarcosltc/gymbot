c3aeb8f2088bfb5479c535d5be9b5548b8edb2c3
unknown <joao163marcos@gmail.com>
2023-06-11 13:02:39 -0300

    mais fixes pequenas

--- src/helpers/Changelog.ts
+++ src/helpers/Changelog.ts
@@ -2,5 +2,6 @@ export default function changelog() {
     return "" +
         "- Adicionado comando !changelog\n" +
         "- Erro ao treinar mais de 5 vezes no total corrigido\n" +
-        "- Easter egg adicionado";
+        "- Easter egg adicionado\n" +
+        "- Coisas pra facilitar a vida do  adicionadas";
 }
